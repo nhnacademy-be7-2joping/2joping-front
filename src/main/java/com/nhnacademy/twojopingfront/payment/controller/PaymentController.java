5fb89492682da75a6041074e6c031e5c313ab2c9
Sauter001 <lsj457@naver.com>
2024-11-07 13:11:32 +0900

    feat: add payment fail page

--- src/main/java/com/nhnacademy/twojopingfront/payment/controller/PaymentController.java
+++ src/main/java/com/nhnacademy/twojopingfront/payment/controller/PaymentController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PaymentController {
-    @GetMapping("/success")
+    @GetMapping("/orders/success")
     public String success(@RequestParam("paymentType") String paymentType,
                           @RequestParam("orderId") String orderId, @RequestParam("paymentKey") String paymentKey,
                           @RequestParam("amount") int amount, Model model) {
@@ -17,5 +17,13 @@ public class PaymentController {
         model.addAttribute("amount", amount);
 
         return "order/payment-success";
+    }
+
+    @GetMapping("/orders/fail")
+    public String fail(@RequestParam("code") String code, @RequestParam("message") String message, Model model) {
+        model.addAttribute("code", code);
+        model.addAttribute("message", message);
+
+        return "order/payment-fail";
     }
 }
