d3a73bec38d1e0b9557ddd35a89291af3db4b6d1
양준하 <dign552@naver.com>
2024-11-11 09:16:27 +0900

    feat: shipmentpolicy front setting

---
+++ src/main/java/com/nhnacademy/twojopingfront/admin/shipment/dto/request/ShipmentPolicyRequestDto.java
@@ -0,0 +1,12 @@
+package com.nhnacademy.twojopingfront.admin.shipment.dto.request;
+
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record ShipmentPolicyRequestDto(
+        @NotBlank String name,
+        @NotNull Integer minOrderAmount,
+        @NotNull Boolean isMemberOnly,
+        @NotNull Integer shippingFee
+) {}
