b140da925b60a6f3ce911cfd877a6d6f29568232
Sauter001 <lsj457@naver.com>
2024-11-13 15:18:56 +0900

    feat: connect books info in customer's cart and wrap info

---
+++ src/main/java/com/nhnacademy/twojopingfront/order/client/WrapClient.java
@@ -0,0 +1,16 @@
+package com.nhnacademy.twojopingfront.order.client;
+
+import com.nhnacademy.twojopingfront.order.dto.response.WrapResponseDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@FeignClient(name = "wrapClient", url = "${gateway.base-url}")
+public interface WrapClient {
+    String prefix = "/v1/wraps";
+
+    @GetMapping(prefix + "/list")
+    ResponseEntity<List<WrapResponseDto>> getAllWraps();
+}
