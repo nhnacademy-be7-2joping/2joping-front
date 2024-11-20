c5a67caa68e4379062187b037f2094d8832bbcae
Sauter001 <lsj457@naver.com>
2024-11-20 09:09:04 +0900

    feat: add interceptor to show current member

--- src/main/java/com/nhnacademy/twojopingfront/common/error/handler/GlobalExceptionHandler.java
+++ src/main/java/com/nhnacademy/twojopingfront/common/error/handler/GlobalExceptionHandler.java
@@ -92,5 +92,4 @@ public class GlobalExceptionHandler {
             return "common/error";
         }
     }
-
 }
--- src/main/java/com/nhnacademy/twojopingfront/common/util/MemberUtils.java
+++ src/main/java/com/nhnacademy/twojopingfront/common/util/MemberUtils.java
@@ -13,7 +13,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
  * </p>
  * <p>
  * ※ 이 클래스는 유틸 클래스로 인스턴스 호출해선 안 됨.
- * 인스턴스화하면 {@code IllegalStateException} 발생함
+ * private이므로 직접적으로 생성자 호출할 수는 없지만, 인스턴스화하면 {@code IllegalStateException} 발생함
  * </p>
  *
  * @author 이승준
@@ -50,7 +50,7 @@ public class MemberUtils {
     /**
      * 현재 접속한 사용자가 익명 사용자(ROLE_ANONYMOUS)인지 확인.
      *
-     * @return 익명 사용자일 경우 true, 그렇지 않을 경우 false
+     * @return 익명 사용자일 경우 {@code true}, 그렇지 않을 경우 {@code false}
      */
     private static boolean isAnonymous() {
         return SecurityContextHolder.getContext().getAuthentication().getAuthorities()
@@ -60,7 +60,7 @@ public class MemberUtils {
     /**
      * 접속 중인 회원의 사용자 ID 가져옴
      *
-     * @return 회원 id. 인증되지 않은 경우 -1
+     * @return 회원 id. 인증되지 않은 경우 {@code -1}
      */
     public static Long getCustomerId() {
         try {
@@ -73,7 +73,7 @@ public class MemberUtils {
     /**
      * 접속 중인 회원의 닉네임을 가져옴
      *
-     * @return 회원의 nickname, 인증되지 않은 경우 빈 문자열
+     * @return 회원의 nickname, 인증되지 않은 경우 빈 문자열({@code ""})
      */
     public static String getNickname() {
         try {
